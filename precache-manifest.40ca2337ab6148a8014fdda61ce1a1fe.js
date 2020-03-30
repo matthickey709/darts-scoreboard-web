f87dfd90ea5efe2d7f460bf6e8e84e851dd69168
Matt Hickey <mjh060@mun.ca>
2020-03-30 15:08:48 -0230

    Updates

---
+++ precache-manifest.40ca2337ab6148a8014fdda61ce1a1fe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43d79d6e33f4398d897a340fde1334c3",
+    "url": "/%7Bdarts-scoreboard-web%7D/index.html"
+  },
+  {
+    "revision": "6bc01f76cff70bfb85f2",
+    "url": "/%7Bdarts-scoreboard-web%7D/static/css/main.fdf90669.chunk.css"
+  },
+  {
+    "revision": "7603c9cd514a45bc3ddb",
+    "url": "/%7Bdarts-scoreboard-web%7D/static/js/2.091f0720.chunk.js"
+  },
+  {
+    "revision": "24b1e13cee46ff35d2b4566989fead9a",
+    "url": "/%7Bdarts-scoreboard-web%7D/static/js/2.091f0720.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bc01f76cff70bfb85f2",
+    "url": "/%7Bdarts-scoreboard-web%7D/static/js/main.0ffee712.chunk.js"
+  },
+  {
+    "revision": "91a4785af2e6ad0cc282",
+    "url": "/%7Bdarts-scoreboard-web%7D/static/js/runtime-main.c62b639f.js"
+  },
+  {
+    "revision": "8773ac057fe2e1e9fcb948a89e2bac9b",
+    "url": "/%7Bdarts-scoreboard-web%7D/static/media/dartboard.8773ac05.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/%7Bdarts-scoreboard-web%7D/static/media/logo.5d5d9eef.svg"
+  }
+]);
