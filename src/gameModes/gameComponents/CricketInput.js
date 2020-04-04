522443d474a889661a4b780721f4c7ec80997b27
Nathaniel Johnson <njj878@mun.ca>
2020-04-04 12:52:27 -0230

    Added empty framework to cricket mode

---
+++ src/gameModes/gameComponents/CricketInput.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+import "../../global";
+
+class AroundTheWorldInput extends React.Component {
+    render() {
+        return (
+            <View style={styles.row}>
+                <Button
+                    title="Increase"
+                    onPress={() => this.props.increaseScoreCallback(this.props.playerNumber)}
+                    color={global.dartboardGreen}
+                />
+                <Text style={styles.scoreDataText}>{this.props.score}</Text>
+                <Button
+                    title="Decrease"
+                    onPress={() => this.props.decreaseScoreCallback(this.props.playerNumber)}
+                    color={global.dartboardRed}
+                />
+            </View>
+        );
+    }
+
+}
+
+const styles = StyleSheet.create({
+    row: {
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 50,
+    },
+    rowData: {
+        flex: 1
+    },
+    scoreDataText: {
+        fontSize: 36,
+        textTransform: "capitalize",
+        color: global.mainTextColor,
+        textAlign: "center",
+    }
+});
+
+export default CricketInput;
