ffe0e956d4d463ba9587e88dc01eb1e2503c0e19
Nathaniel Johnson <njj878@mun.ca>
2020-04-04 15:22:08 -0230

    started updating some styling

--- src/gameModes/gameComponents/CricketInput.js
+++ src/gameModes/gameComponents/CricketInput.js
@@ -3,6 +3,14 @@ import { View, Text, StyleSheet, Button } from "react-native";
 import "../../global";
 
 class CricketInput extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            playerNumber: props.playerNumber,
+            hitNumbers: ""
+        }
+    }
+
     render() {
         return (
             <View style={styles.row}>  
@@ -13,6 +21,39 @@ class CricketInput extends React.Component {
 }
 
 const styles = StyleSheet.create({
+    row: {
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 50,
+    },
+    column: {
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 50,
+    },
+    rowData: {
+        flex: 1
+    },
+    ScoringLevelText: {
+        fontSize: 36,
+        textTransform: "capitalize",
+        color: "white",
+        textAlign: "center",
+        backgroundColor: global.dartboardCork,
+        padding: 5,
+        borderColor: global.mainTextColor,
+        borderWidth: "1px"
+    },
+    playerDataText: {
+        fontSize: 36,
+        textTransform: "capitalize",
+        color: global.mainTextColor,
+        textAlign: "center",
+        backgroundColor: global.dartboardCork,
+        padding: 20
+    }
 });
 
 export default CricketInput;
